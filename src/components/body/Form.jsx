30d64f5ece7d97711f3f8049028e7513620329a2
Aldair André <aldairandre99@gmail.com>
2022-05-09 22:40:42 +0100

    formulario mobile completo

--- src/components/body/Form.jsx
+++ src/components/body/Form.jsx
@@ -1,33 +1,89 @@
 import { useState } from 'react';
+import styled from 'styled-components'
 
+const StyleForm = styled.form`
+    @media(max-width:768px){
+        width:100%;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 5rem;
+    }
+`
+
+const StyleInput = styled.input`
+    @media(max-width:768px){
+        width:40%;
+        margin: 1rem 0px;
+        padding: 15px;
+        font-weight: 900;
+        font-size: 1.2rem;
+        text-align: center;
+        border-radius: 1rem;
+        color: black;
+    }
+    @media(max-width:425px){
+        width:73%;
+    }
+    @media(max-width:375px){
+        width:85%;
+    }
+    @media(max-width:320px){
+        width:100%;
+    }
+`
+
+const StyleInputSubmit = styled.input`
+    @media(max-width:768px){
+        width:30%;
+        margin: 1rem 0px;
+        padding: 15px;
+        font-weight: 900;
+        font-size: 1.2rem;
+        text-align: center;
+        border-radius: 1rem;
+        color: #fff;
+        background:  #000;
+    }
+    @media(max-width:425px){
+        width:63%;
+    }
+    @media(max-width:375px){
+        width:75%;
+    }
+    @media(max-width:320px){
+        width:90%;
+    }
+`
 
 export default function MyForm() {
-  const [name, setName] = useState("");
-  const [peopleNum, setPeopleNum] = useState("");
-  const [time, setTime] = useState("");
+    const [peopleNum, setPeople] = useState("");
+    const [name, setName] = useState("");
+    const [time, setTime] = useState("");
 
-  return (
-    <form>
-        <input
-          type="text" 
-          value={peopleNum}
-          onChange={(e) => setPeopleNum(e.target.value)}
-          placeholder='How many people?'
-        />
-        <input
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder='For how longe?'
-        />
-        <input type="text" value={time}
-          onChange={
-              (e) => {
-                setTime(e.target.value)
-                console.log({time})
-          }}
-          placeholder='Your bast e-mail!'
-        />
-    </form>
-  )
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+    }
+
+    return (
+        <StyleForm onSubmit={handleSubmit}>
+            <StyleInput
+            type="number" 
+            value={peopleNum}
+            onChange={(e) => setPeople(e.target.value)}
+            placeholder='How many people?'
+            />
+            <StyleInput
+            type="number" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder='For how longe?'
+            />
+            <StyleInput type="text" value={time}
+            onChange={(e) => setTime(e.target.value)}
+            placeholder='Your bast e-mail!'
+            />
+            <StyleInputSubmit type="submit" value='Submit'/>
+        </StyleForm>
+    )
 }
--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -1,46 +1,11 @@
-import styled from 'styled-components'
 import MyForm from './Form'
-
-const StyleSubTitle = styled.h2`
-    @media (max-width:768px){
-        font:normal normal 800 2.3rem/normal 'Poppins', sans-serif;
-        text-align: center;
-        margin: 60px 0;
-    }
-    @media(min-width:375px){
-        padding: 0 40px;
-    }
-`
-
-const StyleSubTitle2 = styled(StyleSubTitle)`
-    @media (max-width:768px){
-        font:normal normal 800 2.3rem/normal 'Poppins', sans-serif;
-        margin: 46px 0px 0px 0px;
-    }
-`
-
-const StyleSpan = styled.span`
-    color: #6C63FF;
-`
-const Banner = styled.div`
-    width: 100%;
-    display: flex;
-    @media(max-width:768px){
-        flex-direction: column;
-        align-items: center;
-        justify-content: 
-    }
-`
-
-const Img = styled.img`
-    width: 80%;
-`
+import {StyleSubTitle,StyleSubTitle2,StyleSpan,Banner,Img} from './StyleBody'
 
 export default function Body() {
   return (
     <>
         <Banner>
-            <StyleSubTitle><StyleSpan>Everything</StyleSpan> in one click.</StyleSubTitle>
+            <StyleSubTitle2 as='h2'><StyleSpan>Everything</StyleSpan> in one click.</StyleSubTitle2>
             <Img src='/img/illustration.png' alt=''/>
         </Banner>
         <StyleSubTitle2 as='h3'>Find your <StyleSpan>plin</StyleSpan>!</StyleSubTitle2>
